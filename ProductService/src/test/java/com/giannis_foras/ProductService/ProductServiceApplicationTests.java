b2f0836c6f955e131a7dfdcc4ef9369d159565af
GiannisF12 <44063119+GiannisF12@users.noreply.github.com>
2025-04-15 16:13:06 +0300

    Spring Boot Setup

---
+++ ProductService/src/test/java/com/giannis_foras/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giannis_foras.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
