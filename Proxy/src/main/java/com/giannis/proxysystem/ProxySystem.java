2a365558552cc231e69bbd70f0546ec265d94b8c
GiannisF12 <44063119+GiannisF12@users.noreply.github.com>
2025-04-16 01:09:45 +0300

    Initial commit

--- Proxy/src/main/java/com/giannis/proxysystem/ProxySystem.java
+++ Proxy/src/main/java/com/giannis/proxysystem/ProxySystem.java
@@ -1,19 +1,19 @@
 package com.giannis.proxysystem;
 
-import java.io.*;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
 
 @WebServlet("/proxy-system")
 public class ProxySystem extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
         resp.getWriter().write("System reached");
 
-        // Optional: Forward to swagger UI
         resp.sendRedirect("http://localhost:8081/swagger-ui/index.html");
     }
 }
